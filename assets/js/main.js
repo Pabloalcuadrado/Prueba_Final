2a6e3af8b745d1e094207387c9d1d5543f7c5b61
“Pablosky“ <vinilonegrostore@gmail.com>
2022-09-20 17:36:11 -0300

    se agrega css y js

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+     $("h2").click(function(){
+        $(this).css({
+            "color":"red",
+        })
+    })
+
+    $("h1").hover(function(){
+        $(this).hide("slow")
+    })
+
+
+//toogle de la carta
+$(".card-title").click(function(){
+$(".card-text").toggle ();
+
+})
+
+//prueba alerta
+$("#enviar").click(function(){
+    alert("El correo fre enviado correctamente...");
+
+});
+
+
+
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+})
